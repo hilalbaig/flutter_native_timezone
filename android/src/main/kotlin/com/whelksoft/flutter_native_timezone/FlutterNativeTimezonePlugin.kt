56e80a6c2d10775f496afbfcc3f8deed3b1a7c4a
Baig <hilal.beg@gmail.com>
2025-10-28 13:55:08 +0500

    Refactor FlutterNativeTimezonePlugin for API v2
    Removed backward compatibility code for Flutter API v1 and updated timezone retrieval methods.

--- android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
+++ android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
@@ -4,27 +4,16 @@ import android.os.Build
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
-import java.time.ZoneId
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.time.ZoneId
 import java.util.*
-import kotlin.collections.ArrayList
 
 class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
 
     private lateinit var channel: MethodChannel
-
-    // backward compatibility with flutter api v1
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val plugin = FlutterNativeTimezonePlugin()
-            plugin.setupMethodChannel(registrar.messenger())
-        }
-    }
 
     override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         setupMethodChannel(binding.binaryMessenger)
@@ -37,9 +26,7 @@ class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "getLocalTimezone" -> result.success(getLocalTimezone())
-
             "getAvailableTimezones" -> result.success(getAvailableTimezones())
-
             else -> result.notImplemented()
         }
     }
@@ -54,9 +41,9 @@ class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
 
     private fun getAvailableTimezones(): List<String> {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            ZoneId.getAvailableZoneIds().toCollection(ArrayList())
+            ZoneId.getAvailableZoneIds().toList()
         } else {
-            TimeZone.getAvailableIDs().toCollection(ArrayList())
+            TimeZone.getAvailableIDs().toList()
         }
     }
 
